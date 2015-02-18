4991f4cf2c005558a9e84c61369fabc3696e8150
Rolf Wessels <rolf.wessels@gmail.com>
2015-02-18 07:26:47 +0200

    Adding the login page

--- generators/app/templates/MainSolutionTemplate/src/MainSolutionTemplate.Website/scripts/controllers/loginCtrl.js
+++ generators/app/templates/MainSolutionTemplate/src/MainSolutionTemplate.Website/scripts/controllers/loginCtrl.js
@@ -3,7 +3,30 @@
 /* dashboardCtrl */
 
 angular.module('webapp.controllers')
-  .controller('loginCtrl', ['$scope','$log',	function ($scope, $log) {
-    console.log("load smpl");
+  .controller('loginCtrl', ['$scope','$log','$mdToast',	function ($scope, $log, $mdToast) {
+	$scope.model = {email:"admin", password:"admin!"}
+	$scope.toastPosition = {	    bottom: false,	    top: true,	    left: false,	    right: true	  };
+	$scope.login = login;
+	$scope.forgotPassword = forgotPassword;
+  	
+    function login() {
+    	console.log($scope.model);
+		$mdToast.show(
+	      $mdToast.simple()
+	        .content('Login!')
+	        .position($scope.getToastPosition())
+	        .hideDelay(3000)
+	    );
+    }
+	
+	function forgotPassword() {
+    	$mdToast.show(
+	      $mdToast.simple()
+	        .content('Password!')
+	        .position($scope.getToastPosition())
+	        .hideDelay(3000)
+	    );
+    }
 
   }]);
+
