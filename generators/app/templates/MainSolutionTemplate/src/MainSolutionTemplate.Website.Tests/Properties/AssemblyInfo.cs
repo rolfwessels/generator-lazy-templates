e3bbb77d8a1b48a83eee4bab575286a880c60963
Rolf Wessels <rolf.wessels@gmail.com>
2015-05-17 17:09:57 +0200

    Remove the website tests. This should be handled by js tests

--- generators/app/templates/MainSolutionTemplate/src/MainSolutionTemplate.Website.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("MainSolutionTemplate.Website.Tests")]
-[assembly: AssemblyDescription("Contains all MainSolutionTemplate unit tests")]
-[assembly: MainSolutionTemplate.Utilities.Helpers.Log4NetInitialize("MainSolutionTemplate.Website.Tests")]
-[assembly: log4net.Config.XmlConfigurator]
