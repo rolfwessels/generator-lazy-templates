6cb2ba1fa4f8718be84bd213a5104c171934a1a9
Rolf Wessels <rolf.wessels@gmail.com>
2015-09-24 20:04:52 +0200

    Cleanup the partials and remove the t4!

--- generators/app/templates/MainSolutionTemplate/src/MainSolutionTemplate.Shared/RouteHelper.cs
+++ generators/app/templates/MainSolutionTemplate/src/MainSolutionTemplate.Shared/RouteHelper.cs
@@ -5,14 +5,14 @@
 		public const string ApiPrefix = "api/";
 
         public const string WithId = "{id}";
-        public const string WithDetail = "Detail";
+        public const string WithDetail = "detail";
 
-        public const string UserController = ApiPrefix+"User";
-		public const string UserControllerRegister = "Register";
-		public const string UserControllerLogin = "Login";
-		public const string UserControllerForgotPassword = "ForgotPassword/{email}";
+        public const string UserController = ApiPrefix+"user";
+		public const string UserControllerRegister = "register";
+		public const string UserControllerLogin = "login";
+		public const string UserControllerForgotPassword = "forgotPassword/{email}";
 
-        public const string ProjectController = ApiPrefix + "Project";
+        public const string ProjectController = ApiPrefix + "project";
 
 
         
