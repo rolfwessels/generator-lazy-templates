9b3163ac8369c76ff861555365d45829691bdbcd
Rolf <rolf.wessels@gmail.com>
2017-12-01 07:51:38 +0200

    Fix missing action.

--- generators/app/templates/MainSolutionTemplate/src/MainSolutionTemplate.Api/Startup.cs
+++ generators/app/templates/MainSolutionTemplate/src/MainSolutionTemplate.Api/Startup.cs
@@ -1,28 +1,44 @@
+using System;
+using System.Threading;
 using System.Web.Http.Dependencies;
+using log4net.Config;
 using MainSolutionTemplate.Api.AppStartup;
 using MainSolutionTemplate.Api.Models.Mappers;
 using MainSolutionTemplate.Api.SignalR;
 using MainSolutionTemplate.Api.Swagger;
 using MainSolutionTemplate.Api.WebApi;
+using Microsoft.Owin;
 using Owin;
-using log4net.Config;
 
 namespace MainSolutionTemplate.Api
 {
-	public class Startup
-	{
-	    
-	    public void Configuration(IAppBuilder appBuilder)
-		{
-		    XmlConfigurator.Configure();
-	        CrossOrginSetup.UseCors(appBuilder);
-			BootStrap.Initialize(appBuilder);
-		    MapApi.Initialize();
-			WebApiSetup webApiSetup = WebApiSetup.Initialize(appBuilder, IocApi.Instance.Resolve<IDependencyResolver>());
-			SignalRSetup.Initialize(appBuilder, IocApi.Instance.Resolve<Microsoft.AspNet.SignalR.IDependencyResolver>());
-			SwaggerSetup.Initialize(webApiSetup.Configuration);
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            XmlConfigurator.Configure();
+            CrossOrginSetup.UseCors(appBuilder);
+            BootStrap.Initialize(appBuilder);
+            MapApi.Initialize();
+            var webApiSetup = WebApiSetup.Initialize(appBuilder, IocApi.Instance.Resolve<IDependencyResolver>());
+            SignalRSetup.Initialize(appBuilder,
+                IocApi.Instance.Resolve<Microsoft.AspNet.SignalR.IDependencyResolver>());
+            SwaggerSetup.Initialize(webApiSetup.Configuration);
             SimpleFileServer.Initialize(appBuilder);
             webApiSetup.Configuration.EnsureInitialized();
-		}
-	}
+            RegisterAppDisposing(appBuilder, () => { IocApi.Instance.Container.Dispose(); });
+        }
+
+        #region Private Methods
+
+        private static void RegisterAppDisposing(IAppBuilder appBuilder, Action callback)
+        {
+            var context = new OwinContext(appBuilder.Properties);
+            var token = context.Get<CancellationToken>("host.OnAppDisposing");
+            if (token != CancellationToken.None)
+                token.Register(callback);
+        }
+
+        #endregion
+    }
 }
